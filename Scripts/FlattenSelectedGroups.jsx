8948845967726b7e8f8c0c83ee3ce9e51a1b7645
Nikola Odic <sirgreensock@gmail.com>
2021-03-12 13:05:38 -0500

    Rewriting script for weird layer selections...

--- Scripts/FlattenSelectedGroups.jsx
+++ Scripts/FlattenSelectedGroups.jsx
@@ -34,25 +34,63 @@ function start()
     {
         alert("Error: No layers are selected!");
     }
-
+    
     for( i = 0; i < selectedLayers.length; i++)
     {
         if (selectedLayers[i].typename === "LayerSet")
         {
-            selectedLayers[i].selected = true;
-            selectedLayers[i].merge();
+            app.activeDocument.activeLayer = selectedLayers[i];
+
+            //Photoshop can only select layers by name, so we need to ensure the name is unique...
+            var oldName = selectedLayers[i].name;
+            var newName = selectedLayers[i].name + Math.random().toString(36).substring(2, 15);
+            
+            //Rename, select by name, rename back to cached name
+            selectedLayers[i].name = newName;
+            selectLayerName(newName);
+            selectedLayers[i].name = oldName;
+
+            app.activeDocument.activeLayer.merge();          
+
         }
         else
         {
             continue;
         }  
     } 
+    
 }
+
+//ActionReference function to select a layer by name
+function selectLayerName(name)
+{
+    var idslct = charIDToTypeID( "slct" );
+    var desc5 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref1 = new ActionReference();
+        var idLyr = charIDToTypeID( "Lyr " );
+        ref1.putName( idLyr, name );
+    desc5.putReference( idnull, ref1 );
+    var idMkVs = charIDToTypeID( "MkVs" );
+    desc5.putBoolean( idMkVs, false );
+    var idLyrI = charIDToTypeID( "LyrI" );
+        var list1 = new ActionList();
+        list1.putInteger( 2 );
+    desc5.putList( idLyrI, list1 );
+executeAction( idslct, desc5, DialogModes.NO );
+}
+
 
 //Runs undo function
 function undo() {
     executeAction(cTID("undo", undefined, DialogModes.NO));
  }; 
+
+ function fuckme()
+ {
+     executeAction(cTID = function(s) { return app.charIDToTypeID(s); });
+     
+ }
 
 //Creates group from selected layers
 //pushes contents of group to an array
@@ -66,8 +104,7 @@ function getSelectedLayers(parentDoc) {
   var layers = group.layers;
 
   for (var i = 0; i < layers.length; i++) {
-    selLayers.push(layers[i]);
-    layers[i].selected = false;
+    selLayers.push(layers[i]);    
   }
 
   undo();
@@ -86,6 +123,29 @@ function newGroupFromLayers(parentDoc) {
     desc.putReference( cTID('From'), lref);
     executeAction( cTID('Mk  '), desc, DialogModes.NO );
 };
+
+function deselectAllLayers(parentDoc)
+{
+    var idselectNoLayers = stringIDToTypeID( "selectNoLayers" );
+
+    var desc26 = new ActionDescriptor();
+
+    var idnull = charIDToTypeID( "null" );
+
+        var ref3 = new ActionReference();
+
+        var idLyr = charIDToTypeID( "Lyr " );
+
+        var idOrdn = charIDToTypeID( "Ordn" );
+
+        var idTrgt = charIDToTypeID( "Trgt" );
+
+        ref3.putEnumerated( idLyr, idOrdn, idTrgt );
+
+    desc26.putReference( idnull, ref3 );
+
+    executeAction( idselectNoLayers, desc26, DialogModes.NO );
+}
 
 //these functions are called by various ScriptListener-based functions
 function cTID(s) {return app.charIDToTypeID(s);}
